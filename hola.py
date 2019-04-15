db75d206cb64c6014ea8e0228ed29f9992bd0a80
BissusJX <bissus17@gmail.com>
2019-04-15 16:29:14 -0500

    python

---
+++ hola.py
@@ -0,0 +1,5 @@
+"""
+    Este es mi primer ejemplo
+    en lenguaje python
+"""
+print("WINTER IS HERE") print("GAME OF THRONES")
---
+++ presentacion.py
@@ -0,0 +1,9 @@
+"""
+  Esta es mi primer trabajo
+  individual en python
+ """
+
+nombre="Diana"
+apellido="Serrano"
+print(nombre)
+print(apellido)
